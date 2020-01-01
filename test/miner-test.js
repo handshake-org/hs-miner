ae2a4cee3cce73d6af86365f7d12bf9c026f0c38
Mark Tyneway <mark@purse.io>
2019-12-31 21:01:07 -0800

    test: add a miner test

---
+++ test/miner-test.js
@@ -0,0 +1,26 @@
+'use strict';
+
+const assert = require('assert');
+const Miner = require('../bin/miner');
+
+const miner = new Miner({
+  backend: Miner.BACKEND,
+  target: Miner.TARGET,
+  threads: 16
+});
+
+describe('Miner', function() {
+  it('should mine a valid block', async () => {
+    const job = await miner.getJob();
+    const [header, target] = job;
+
+    const [nonce, valid] = await miner.mine(header, target);
+
+    assert(valid);
+
+    const hdr = miner.toBlock(header, nonce);
+    const hash = miner.hashHeader(hdr);
+
+    assert(miner.verify(hdr, Miner.TARGET));
+  });
+});
