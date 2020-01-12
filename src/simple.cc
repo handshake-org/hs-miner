423e97a5768010ac56307cf9cd36d87be2ef49be
Matthew Zipkin <pinheadmz@gmail.com>
2020-01-12 13:20:11 -0500

    simple: search nonce space in parrallel with threads

--- src/simple.cc
+++ src/simple.cc
@@ -11,6 +11,7 @@ typedef struct hs_thread_args_s {
   hs_options_t *options;
   uint32_t *result;
   bool *match;
+  uint8_t thread;
 } hs_thread_args_t;
 
 void
@@ -19,6 +20,7 @@ void
   hs_options_t *options = args->options;
   uint32_t *result = args->result;
   bool *match = args->match;
+  uint8_t thread = args->thread;
 
   uint32_t nonce = 0;
   uint32_t range = 1;
@@ -31,7 +33,11 @@ void
   if (options->range)
     range = options->range;
 
-  uint32_t max = nonce + range;
+  // Split up the range into threads and start
+  // this thread's nonce from a unique point in the range.
+  uint32_t sub_range = range / options->threads;
+  nonce += sub_range * thread;
+  uint32_t max = nonce + sub_range;
 
   if (header_len != HEADER_SIZE)
     return (void *)HS_EBADARGS;
@@ -55,12 +61,14 @@ void
   for (; nonce < max; nonce++) {
     if (!options->running)
       break;
-    // printf("Thread: %u Nonce: %zu\n", pthread_self(), nonce);
 
     // Insert nonce into share
     memcpy(share, &nonce, 4);
 
     hs_header_share_pow(share, pad32, hash);
+
+    // printf("Thread: %u Nonce: %zu Hash: %02x%02x%02x%02x\n",
+    //   thread, nonce, hash[0], hash[1], hash[2], hash[3]);
 
     if (memcmp(hash, target, 32) <= 0) {
       *match = true;
@@ -78,17 +86,23 @@ hs_simple_run(
   uint32_t *result,
   bool *match
 ) {
-  hs_thread_args_t args;
-  args.options = options;
-  args.result = result;
-  args.match = match;
-
   uint8_t NUM_THREADS = options->threads;
   pthread_t threads[NUM_THREADS];
 
+  // Array of args structs for each thread
+  hs_thread_args_t *thread_args[NUM_THREADS];
+
   for(uint8_t i = 0; i < NUM_THREADS; i++) {
-    // printf("Creating thread: %u\n", i);
-    int err = pthread_create(&threads[i], NULL, hs_simple_thread, &args);
+    // Create new args object in memory for each thread so we can add
+    // thread IDs to it and return different nonces.
+    thread_args[i] = (hs_thread_args_t *)malloc(sizeof(hs_thread_args_t));
+    thread_args[i]->options = options;
+    thread_args[i]->result = result;
+    thread_args[i]->match = match;
+    thread_args[i]->thread = i;
+
+    int err =
+      pthread_create(&threads[i], NULL, hs_simple_thread, thread_args[i]);
     if (err != 0) {
       printf("Error creating thread: %u\n", err);
       exit(err);
@@ -98,7 +112,8 @@ hs_simple_run(
   for(uint8_t i = 0; i < NUM_THREADS; i++) {
     int32_t *rc;
     pthread_join(threads[i], (void **)&rc);
-    // printf("Return code from thread %u: %zu\n", i, rc);
+    if (rc != 0)
+      printf("Thread %u returned error code: %zu\n", i, rc);
   }
   return HS_SUCCESS;
 }
